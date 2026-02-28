af59ef725ae42502a308a2953107148f92a10968
Staley <staleylane101@gmail.com>
2026-02-28 10:50:04 +0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+/*
+Name: Staley Lane T. Cardeno
+Date: 2/28/2026
+Project Description: A console based string-processing system that allows users to input text and perform various string operations using built-in C# String methods. 
+**/
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    StringView view = new StringView();
+    view.Run();
+  }
